75f8bdc7670fbf72ccb21de9eab680fc7641869e
Aluno04-2 <maria.helenamm2025@gmail.com>
2025-04-10 08:59:18 -0300

    Segunda commit.

---
+++ php/trabalho.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>horarios</title>
+</head>
+
+
+<p>EEEP Joaquim Nogueira</p>
+    <h1>1° INFORMÁTICA</h1>
+    <H6>Nome da Escola: <h5 align="right">Professor Coordenador: : LAIS</h5></H6>
+
+
+<body>
+<div>
+    <table border="1">
+
+        <tr>
+            <td colspan="2" >sla</td>
+            <td>1</td>
+            <td>2</td>
+            <td>intervalo</td>
+        </tr>
+
+
+    </table>
+</div>
+
+<?php 
+
+$teste = "testando";
+echo "$teste";
+ ?>
+ <form name="sla" action="primeirophp.php"></form>
+
+ nome:<input type="texto"placeholder="digite algo">
+ enp:<input type="submit"placeholder="digite algo">
+ fone:<input placeholder="digite algo">
+ ok:<input>
+
+</body>
+</html>
